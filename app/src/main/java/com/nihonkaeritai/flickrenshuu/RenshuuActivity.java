9034d37e262e90b68a30ec8b6b5e5178d310f801
Benjamin Warner <benjaminnwarner@gmail.com>
2016-03-05 19:49:27 -0500

    Refactoring on RenshuuActivity

--- app/src/main/java/com/nihonkaeritai/flickrenshuu/RenshuuActivity.java
+++ app/src/main/java/com/nihonkaeritai/flickrenshuu/RenshuuActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
-import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,7 +18,7 @@ import com.nihonkaeritai.flickrenshuu.utility.FancyCountdownTimer;
 public class RenshuuActivity extends AppCompatActivity {
     private FancyCountdownTimer timer;
     private TextView kanaKey;
-    private TextView chisaiIndicator;
+    private KanaRepository kanaRepository;
     private EditText userInput;
     private final long DURATION = 3000;
 
@@ -28,93 +27,74 @@ public class RenshuuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_renshuu);
 
-        kanaKey = (TextView)findViewById(R.id.kanaKey);
+        kanaKey = (TextView) findViewById(R.id.kanaKey);
 
-        chisaiIndicator = (TextView)findViewById(R.id.chisaiIndicator);
-        chisaiIndicator.setVisibility(TextView.INVISIBLE);
-
-        userInput = (EditText)findViewById(R.id.userInput);
+        userInput = (EditText) findViewById(R.id.userInput);
         userInput.setVisibility(EditText.INVISIBLE);
 
-        KanaRepository.initialize(this);
+        kanaRepository = new KanaRepository(this);
 
         waitForTapToStart();
     }
 
-    private void setVisible(){
-        userInput.setVisibility(EditText.VISIBLE);
-        chisaiIndicator.setVisibility(TextView.VISIBLE);
-    }
-
     private void waitForTapToStart() {
-        final RelativeLayout tapToStart = (RelativeLayout)findViewById(R.id.mainLayout);
+        final RelativeLayout tapToStart = (RelativeLayout) findViewById(R.id.mainLayout);
         tapToStart.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 TextView startMessage = (TextView) findViewById(R.id.pushToStartText);
+
                 startMessage.setVisibility(TextView.INVISIBLE);
-                setVisible();
+                userInput.setVisibility(EditText.VISIBLE);
+
                 startTextWatcher();
-                startTimer();
-                generateRandomKana();
+                initTimer();
+                kanaKey.setText(kanaRepository.getNextKana());
+                timer.start();
                 return false;
             }
         });
     }
 
-    private void startTimer() {
-        timer = new FancyCountdownTimer(DURATION,this){
+    private void initTimer() {
+        timer = new FancyCountdownTimer(DURATION, this) {
             @Override
             public void onFinish() {
                 userInput.setText("");
-                timer.setDisplay("0:00");
-                generateRandomKana();
+                timer.start();
+                kanaKey.setText(kanaRepository.getNextKana());
             }
         };
     }
 
     private void startTextWatcher() {
-        if(userInput.requestFocus()) {
+        if (userInput.requestFocus()) {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.showSoftInput(userInput, InputMethodManager.SHOW_IMPLICIT);
         }
         userInput.addTextChangedListener(new TextWatcher() {
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
+
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() > 0) {
-                    compareInputToKey(s.toString());
+                if (s.length() > 0) {
+                    handleInputtedText(s.toString());
                 }
             }
         });
     }
 
-    private void compareInputToKey(String userInputAsString) {
-        String kanaKeyAsString = kanaKey.getText().toString();
-        if(userInputAsString.equals(kanaKeyAsString)) {
-            clearInputField();
-            generateRandomKana();
-        }else if(userInputAsString.length() > kanaKeyAsString.length()){
-            clearInputField();
+    private void handleInputtedText(String input) {
+        if(kanaRepository.inputIsEqualToKey(input)){
+            kanaKey.setText(kanaRepository.getNextKana());
+            userInput.setText("");
+            timer.start();
         }
-    }
-
-    private void generateRandomKana(){
-        String nextKana = KanaRepository.getNextKana();
-        kanaKey.setText(nextKana);
-
-        if(KanaRepository.isIndexChisaiKana()){
-            chisaiIndicator.setVisibility(View.VISIBLE);
-        }else{
-            chisaiIndicator.setVisibility(View.INVISIBLE);
-        }
-        timer.start();
-    }
-
-    private void clearInputField(){
-        userInput.setText("");
     }
 }
--- app/src/main/java/com/nihonkaeritai/flickrenshuu/repositories/KanaRepository.java
+++ app/src/main/java/com/nihonkaeritai/flickrenshuu/repositories/KanaRepository.java
@@ -1,20 +1,23 @@
 package com.nihonkaeritai.flickrenshuu.repositories;
 
-import android.content.Context;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.nihonkaeritai.flickrenshuu.R;
 
 import java.util.Random;
 
 public class KanaRepository {
-    private static final int NUMBER_OF_CHISAI_KANA = 8;
-    private static String[] kanaArray;
-    private static int[] kanaOrder;
-    private static int currentKana;
+    private Kana[] kanaDictionary;
+    private int[] kanaOrder;
+    private int currentKana;
+    private TextView chisaiIndicator;
 
-    public static void initialize(Context context){
-        kanaArray = context.getResources().getStringArray(R.array.kana);
+    public KanaRepository(AppCompatActivity context){
+        initializeKanaDictionary(context);
+
+        chisaiIndicator = (TextView)context.findViewById(R.id.chisaiIndicator);
 
         kanaOrder = new int[80];
         for(int i = 0; i < 80; i++){
@@ -23,17 +26,48 @@ public class KanaRepository {
         currentKana = kanaOrder.length - 1;
     }
 
-    public static String getNextKana(){
+    private void initializeKanaDictionary(AppCompatActivity context){
+        String[] kanaArray;
+        kanaArray = context.getResources().getStringArray(R.array.kana);
+
+        kanaDictionary = new Kana[80];
+        for(int i = 0; i < kanaArray.length; i++){
+            Kana kana = new Kana();
+            kana.kana = kanaArray[i];
+            //46-70 have diacritical
+            if(i >= 46 && i <= 70)
+                kana.hasDiacritical = true;
+            //71-79 are chisai
+            else if( i >= 71)
+                kana.isChisai = true;
+            else{
+                kana.hasDiacritical = false;
+                kana.isChisai = false;
+            }
+            kanaDictionary[i] = kana;
+        }
+    }
+
+    public String getNextKana(){
         if(currentKana == kanaOrder.length - 1)
             generateOrderCombination();
-        String thisKana = kanaArray[kanaOrder[currentKana]];
+        if(kanaDictionary[kanaOrder[currentKana]].isChisai)
+            chisaiIndicator.setVisibility(View.VISIBLE);
+        else
+            chisaiIndicator.setVisibility(View.INVISIBLE);
+
+        String thisKana = kanaDictionary[kanaOrder[currentKana]].kana;
         currentKana++;
         return thisKana;
     }
 
-    private static void generateOrderCombination(){
+    public boolean inputIsEqualToKey(String input){
+        return input.equals(kanaDictionary[kanaOrder[currentKana-1]].kana);
+    }
+
+    private void generateOrderCombination(){
         Random random = new Random();
-        for(int i = kanaArray.length - 1; i > 0; i--){
+        for(int i = kanaDictionary.length - 1; i > 0; i--){
             int randomIndex = random.nextInt((i+1));
             int old = kanaOrder[randomIndex];
             kanaOrder[randomIndex] = kanaOrder[i];
@@ -42,7 +76,9 @@ public class KanaRepository {
         currentKana = 0;
     }
 
-    public static boolean isIndexChisaiKana(){
-        return kanaOrder[currentKana-1] >= kanaArray.length - 1 - NUMBER_OF_CHISAI_KANA;
+    private class Kana{
+        public String kana;
+        public boolean isChisai;
+        public boolean hasDiacritical;
     }
 }
