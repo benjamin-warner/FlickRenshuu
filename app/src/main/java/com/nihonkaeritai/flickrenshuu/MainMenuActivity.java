602c9a4073d5988234bce401a58f3df733753168
Benjamin Warner <benjaminnwarner@gmail.com>
2016-03-26 18:54:01 -0400

    removed unused import statements

--- app/src/main/java/com/nihonkaeritai/flickrenshuu/MainMenuActivity.java
+++ app/src/main/java/com/nihonkaeritai/flickrenshuu/MainMenuActivity.java
@@ -3,8 +3,6 @@ package com.nihonkaeritai.flickrenshuu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
